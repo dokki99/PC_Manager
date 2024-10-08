cc277459792f1dda06b8fe0bc8adc5ff5c734f46
jeong hyun <kjyukman4@gmail.com>
2024-10-08 13:29:38 +0900

    서버 오픈 코드 및 DB 연결 코드 업로드

---
+++ Server/main.cpp
@@ -0,0 +1,507 @@
+#include <windows.h>
+#include <sql.h>
+#include <sqlext.h>
+#include <commctrl.h>
+#include "resource.h"
+
+#define ID_TOOLBAR 100
+
+
+// 소켓 정보 구조체
+typedef struct Client_Socket {
+	TCHAR ID[128];
+	SOCKET Client_Sock;
+	struct Client_Socket* link;
+}CS;
+
+void Init_Wnd(WNDCLASS*, int);
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK Stock_Info_Proc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK Revenue_Info_Proc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK Employee_Info_Proc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK Customer_Info_Proc(HWND, UINT, WPARAM, LPARAM);
+
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("카운터");
+LPCTSTR S_Class = TEXT("재고");
+LPCTSTR R_Class = TEXT("매출");
+LPCTSTR E_Class = TEXT("직원");
+LPCTSTR C_Class = TEXT("회원");
+HWND hWndMain;		// 메인 화면 핸들
+HWND hWnd_S;		// 재고정보 화면 핸들
+HWND hWnd_R;		// 매출정보 화면 핸들
+HWND hWnd_E;		// 직원정보 화면 핸들
+HWND hWnd_C;		// 회원정보 화면 핸들
+HWND hToolBar;		// 툴바 핸들
+
+// DB관련 함수///////////////////////////////////////////////
+
+BOOL DBConnect();
+void DBDisconnect();
+BOOL DBExcuteSQL();
+
+/////////////////////////////////////////////////////////////
+
+
+// DB관련 변수///////////////////////////////////////////////
+TCHAR ID[10] = "kjyukman";
+TCHAR PWD[10] = "";
+SQLHENV hEnv;
+SQLHDBC hDbc;
+SQLHSTMT hStmt;
+
+/////////////////////////////////////////////////////////////
+
+
+// 소켓관련 변수/////////////////////////////////////////////
+
+SOCKET listensock;
+SOCKET clientsock = 0;
+CS* C_S;									// 소켓 Linked		
+sockaddr_in addr_server;
+sockaddr_in addr_client;
+int addrlen_clt = sizeof(sockaddr);
+int nReturn;
+WSADATA wsadata;
+
+/////////////////////////////////////////////////////////////
+
+
+// 소켓관련 함수/////////////////////////////////////////////
+
+CS* create();							// 소켓 구조체 할당
+void addsock(SOCKET);					// 소켓 정보 추가
+void delsock(SOCKET);					// 소켓 정보 삭제
+
+/////////////////////////////////////////////////////////////
+
+
+// 서버관련 함수/////////////////////////////////////////////
+
+void SVR_Open();						// 서버 오픈
+DWORD WINAPI Connect_Thread(LPVOID);	// 서버 연결 스레드
+DWORD WINAPI Recv_Thread(LPVOID);		// 수신 스레드
+
+/////////////////////////////////////////////////////////////
+
+// 서버관련 변수/////////////////////////////////////////////
+
+BOOL CONN_ST = FALSE;						// 서버 오픈 상태
+const unsigned short g_uPort = 7878;		// 포트 넘버
+const int buflen = 4096;					// 버퍼 크기
+TCHAR buf[buflen];
+
+/////////////////////////////////////////////////////////////
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpszCmdParam, int nCmdShow) {
+
+	if (DBConnect() == FALSE) {
+		MessageBox(NULL, (LPCSTR)"데이터 베이스에 연결할 수 없습니다.", (LPCSTR)"에러", MB_OK);
+		return 0;
+	}
+
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = CreateSolidBrush(RGB(222, 225, 218));
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_ASTERISK);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	DBDisconnect();
+
+	return (int)Message.wParam;
+
+}
+
+void Init_Wnd(WNDCLASS* Wnd, int Proc_No) {
+	if(Proc_No > 3){
+		MessageBox(hWndMain, "윈도우 초기화 오류!", "알림", MB_OK);
+		return;
+	}
+
+	Wnd->cbClsExtra = 0;
+	Wnd->cbWndExtra = 0;
+	Wnd->hbrBackground = (HBRUSH)RGB(0, 0, 0);
+	Wnd->hCursor = LoadCursor(NULL, IDC_ARROW);
+	Wnd->hIcon = LoadIcon(NULL, IDI_ASTERISK);
+	Wnd->hInstance = g_hInst;
+	switch (Proc_No)
+	{
+	case 0:
+		Wnd->lpfnWndProc = Stock_Info_Proc;
+		Wnd->lpszClassName = S_Class;
+		break;
+	case 1:
+		Wnd->lpfnWndProc = Revenue_Info_Proc;
+		Wnd->lpszClassName = R_Class;
+		break;
+	case 2:
+		Wnd->lpfnWndProc = Employee_Info_Proc;
+		Wnd->lpszClassName = E_Class;
+		break;
+	case 3:
+		Wnd->lpfnWndProc = Customer_Info_Proc;
+		Wnd->lpszClassName = C_Class;
+		break;
+	}
+	Wnd->lpszMenuName = NULL;
+	Wnd->style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(Wnd);
+}
+
+/*--------------------------------------------------------
+ WndProc: 메인 윈도우 프로시저
+--------------------------------------------------------*/
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+
+	switch (iMessage) {
+	case WM_CREATE:
+		hWndMain = hWnd;
+		InitCommonControls();
+
+		C_S = create();		// 소켓 구조체 생성
+
+		WNDCLASS Wnd_S;
+		WNDCLASS Wnd_R;
+		WNDCLASS Wnd_E;
+		WNDCLASS Wnd_C;
+
+		// 제품윈도우 설정
+		Init_Wnd(&Wnd_S, 0);
+		// 매출윈도우 설정
+		Init_Wnd(&Wnd_R, 1);
+		// 직원윈도우 설정
+		Init_Wnd(&Wnd_E, 2);
+		// 회원윈도우 설정
+		Init_Wnd(&Wnd_C, 3);
+
+		DBExcuteSQL();
+		
+		return 0;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_CONNECT:
+			SVR_Open();		// 서버오픈
+			break;
+		case ID_S_INFO:		// 재고(Stock)정보 오픈
+			hWnd_S = CreateWindow(S_Class, S_Class, WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, g_hInst, NULL);
+			ShowWindow(hWnd_S, SW_SHOW);
+			break;
+		case ID_R_INFO:		// 매출(Revenue)정보 오픈
+			hWnd_R = CreateWindow(R_Class, R_Class, WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, g_hInst, NULL);
+			ShowWindow(hWnd_R, SW_SHOW);
+			break;
+		case ID_E_INFO:		// 직원(Employee)정보 오픈
+			hWnd_E = CreateWindow(E_Class, E_Class, WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, g_hInst, NULL);
+			ShowWindow(hWnd_E, SW_SHOW);
+			break;
+		case ID_C_INFO:		// 회원(Customer)정보 오픈
+			hWnd_C = CreateWindow(C_Class, C_Class, WS_VISIBLE | WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, g_hInst, NULL);
+			ShowWindow(hWnd_C, SW_SHOW);
+			break;
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+/*--------------------------------------------------------
+ Stock_Info_Proc: 재고정보 프로시저
+--------------------------------------------------------*/
+LRESULT CALLBACK Stock_Info_Proc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+	case WM_CREATE:
+		return 0;
+	case WM_DESTROY:
+		break;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+/*--------------------------------------------------------
+ Revenue_Info_Proc: 매출정보 프로시저
+--------------------------------------------------------*/
+LRESULT CALLBACK Revenue_Info_Proc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+	case WM_CREATE:
+		return 0;
+	case WM_DESTROY:
+		break;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+/*--------------------------------------------------------
+ Employee_Info_Proc: 직원정보 프로시저
+--------------------------------------------------------*/
+LRESULT CALLBACK Employee_Info_Proc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam){
+	switch (iMessage) {
+	case WM_CREATE:
+		return 0;
+	case WM_DESTROY:
+		break;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+/*--------------------------------------------------------
+ Customer_Info_Proc: 회원정보 프로시저
+--------------------------------------------------------*/
+LRESULT CALLBACK Customer_Info_Proc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	switch (iMessage) {
+	case WM_CREATE:
+		return 0;
+	case WM_DESTROY:
+		break;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+
+/*--------------------------------------------------------
+ DBConnect(): DB 연결
+--------------------------------------------------------*/
+BOOL DBConnect() {
+	// 연결 설정을 위한 변수
+	SQLRETURN Ret;
+
+	// 환경 핸들을 할당하고 버전 속성을 설정한다.
+	if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv) != SQL_SUCCESS) return FALSE;
+	if (SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, SQL_IS_INTEGER) != SQL_SUCCESS) return FALSE;
+
+	// 연결 핸들을 할당하고 연결한다
+	if (SQLAllocHandle(SQL_HANDLE_DBC, hEnv, &hDbc) != SQL_SUCCESS) return FALSE;
+	Ret = SQLConnect(hDbc, (SQLCHAR*)"PCDB", SQL_NTS, (SQLCHAR*)ID, SQL_NTS, (SQLCHAR*)PWD, SQL_NTS);
+	if ((Ret != SQL_SUCCESS) && (Ret != SQL_SUCCESS_WITH_INFO)) return FALSE;
+
+	// 명령 핸들을 할당한다.
+	if (SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt) != SQL_SUCCESS) return FALSE;
+
+	return TRUE;
+}
+
+
+/*--------------------------------------------------------
+ DBDisConnect(): DB 해제
+--------------------------------------------------------*/
+void DBDisconnect() {
+	// 뒷정리
+	if (hStmt) SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
+	if (hDbc) SQLDisconnect(hDbc);
+	if (hDbc) SQLFreeHandle(SQL_HANDLE_DBC, hDbc);
+	if (hEnv) SQLFreeHandle(SQL_HANDLE_ENV, hEnv);
+}
+
+/*--------------------------------------------------------
+ DBExcuteSQL(): 데이터 가져오기
+--------------------------------------------------------*/
+BOOL DBExcuteSQL() {
+	// 결과값을 돌려받기 위한 변수들
+	SQLCHAR S_Code[30], S_Name[30], S_Category[30];
+	int S_Price, S_Amount;
+	double S_Margin;
+	SQLLEN I_S_Code, I_S_Name, I_S_Category, I_S_Price, I_S_Amount, I_S_Margin;
+
+	// 화면 출력을 위한 변수들
+	int y = 1;
+	HDC hdc;
+	int arTab[2] = { 200,400 };
+	TCHAR str[255];
+
+	// 결과를 돌려받기 위해 바인딩한다.
+	SQLBindCol(hStmt, 1, SQL_C_CHAR, S_Code, sizeof(S_Code), &I_S_Code);
+	SQLBindCol(hStmt, 2, SQL_C_CHAR, S_Name, sizeof(S_Name), &I_S_Name);
+	SQLBindCol(hStmt, 3, SQL_C_CHAR, S_Category, sizeof(S_Category), &I_S_Category);
+	SQLBindCol(hStmt, 4, SQL_C_ULONG, &S_Price, 0, &I_S_Price);
+	SQLBindCol(hStmt, 5, SQL_C_ULONG, &S_Amount, 0, &I_S_Amount);
+	SQLBindCol(hStmt, 6, SQL_C_ULONG, &S_Margin, 0, &I_S_Margin);
+
+	// SQL문을 실행한다
+	if (SQLExecDirect(hStmt, (SQLCHAR*)"select S_Code, S_Name, S_Category, S_Price, S_Amount, S_Margin from dbo.Stock_Info", SQL_NTS) != SQL_SUCCESS) return FALSE;
+
+	// 읽어온 데이터 출력
+	hdc = GetDC(hWndMain);
+	while (SQLFetch(hStmt) != SQL_NO_DATA) {
+		wsprintf(str, "이름:%s\t가격:%d", S_Name, S_Price);
+		MessageBox(hWndMain, str, "", MB_OK);
+	}
+
+	ReleaseDC(hWndMain, hdc);
+
+	if (hStmt) SQLCloseCursor(hStmt);
+	return TRUE;
+}
+
+
+/*--------------------------------------------------------
+ create(): 소켓 구조체 할당
+--------------------------------------------------------*/
+CS* create() {
+	CS* N;
+
+	N = (CS*)malloc(sizeof(CS));
+
+	lstrcpy(N->ID, TEXT(""));
+	N->Client_Sock = 0;
+	N->link = NULL;
+
+	return N;
+}
+
+/*--------------------------------------------------------
+ SVR_Open(): 서버 오픈
+--------------------------------------------------------*/
+void SVR_Open() {
+	static BOOL state = TRUE;
+	HANDLE hThread;
+	DWORD ThreadID;
+
+	if (state) {
+		// 소켓 초기화 (윈속 라이브러리 버전, 윈속 시스템 관련 정보)
+		nReturn = WSAStartup(WORD(2.0), &wsadata);
+
+		// 소켓 생성 (IPv4: AF_INET | IPv6: AF_INET6 , 소켓 통신 타입, 프로토콜 결정)
+		listensock = socket(AF_INET, SOCK_STREAM, 0);
+
+		addr_server.sin_family = AF_INET;
+		addr_server.sin_addr.s_addr = htons(INADDR_ANY);
+		addr_server.sin_port = htons(g_uPort);
+
+		// 소켓 바인드 (소켓 객체, 소객 객체에 부여할 주소 정보 구조체, 구조체 길이)
+		nReturn = bind(listensock, (sockaddr*)&addr_server, sizeof(sockaddr));
+
+		// 접속 대기 (소켓 객체, 연결 대기열 크기)
+		nReturn = listen(listensock, 1);
+
+		// accept 스레드 생성
+		CloseHandle(hThread = CreateThread(NULL, 0, Connect_Thread, &listensock, 0, &ThreadID));
+
+		CONN_ST = TRUE;
+
+		state = FALSE;
+	}
+	InvalidateRect(hWndMain, NULL, FALSE);
+	MessageBox(hWndMain, "서버오픈완료", "알림", MB_OK);
+}
+
+
+/*--------------------------------------------------------
+ Connect_Thread(LPVOID): 서버 연결 스레드
+--------------------------------------------------------*/
+DWORD WINAPI Connect_Thread(LPVOID Param) {
+	SOCKET* P = (SOCKET*)Param;
+	HANDLE hThread;
+	DWORD ThreadID;
+	CS* S;
+
+	// 접속 허용하기 (소켓 객체, 클라이언트 주소 정보, 주소 정보 구조 크기)
+	for (;;) {
+		clientsock = accept(*P, (sockaddr*)&addr_client, &addrlen_clt);
+
+		if (clientsock != INVALID_SOCKET) {
+			MessageBox(hWndMain, "클라이언트가 접속하였습니다.", "server", MB_OK);
+
+			addsock(clientsock);
+
+			S = C_S;
+			while (S->link != NULL) {
+				S = S->link;
+			}
+
+			CloseHandle(hThread = CreateThread(NULL, 0, Recv_Thread, &(S->Client_Sock), 0, &ThreadID));
+		}
+
+	}
+	return 0;
+}
+
+/*--------------------------------------------------------
+ Recv_Thread(LPVOID): 수신 스레드
+--------------------------------------------------------*/
+DWORD WINAPI Recv_Thread(LPVOID Param) {
+	SOCKET* P = (SOCKET*)Param;
+
+
+	static int i;
+
+	while (1) {
+		// 데이터 수신 (소캣 객체, 받을 문자열, 문자열 크기, 옵션);
+		nReturn = recv(*P, buf, buflen, 0);
+
+		if (nReturn == 0 || nReturn == SOCKET_ERROR) {
+			MessageBox(hWndMain, "수신에러", "server", MB_OK);
+			return 0;
+		}
+		else {
+		}
+	}
+	return 0;
+}
+
+/*--------------------------------------------------------
+ addsock(SOCKET): 소켓 정보 추가
+--------------------------------------------------------*/
+void addsock(SOCKET S) {
+	CS* N, * P;
+
+	N = create();
+	N->Client_Sock = S;
+
+	P = C_S;
+
+	while (P->link != NULL) {
+		P = P->link;
+	}
+
+	P->link = N;
+
+}
+
+/*--------------------------------------------------------
+ delsock(SOCKET): 소켓 정보 삭제
+--------------------------------------------------------*/
+void delsock(SOCKET S) {
+	if (C_S->link != NULL) {
+		CS* E, * P;
+		P = C_S;
+
+		while (P->link != NULL) {
+			E = P;
+			P = P->link;
+			if (P->Client_Sock == S) {
+				E->link = P->link;
+				break;
+			}
+		}
+		free(P);
+	}
+}
---
+++ Server/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Server.rc에서 사용되고 있습니다.
+//
+#define IDB_OPEN                        101
+#define IDR_MENU                        102
+#define ID_CONNECT                      40003
+#define ID_DISCONNECT                   40004
+#define ID_E_INFO                       40015
+#define ID_C_INFO                       40016
+#define ID_R_INFO                       40017
+#define ID_S_INFO                       40018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
