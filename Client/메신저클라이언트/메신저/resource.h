2b5db0df0f0c59e7bb055f6b17f5c8ec18286dd4
winjuuuun <hsj1128ty@naver.com>
2024-10-15 03:29:08 +0900

    불필요한 부분 정리

--- Client/메신저클라이언트/메신저/resource.h
+++ Client/메신저클라이언트/메신저/resource.h
@@ -2,64 +2,51 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // 메신저.rc에서 사용되고 있습니다.
 //
-#define IDD_JOINLOGINDIALOG             9
-#define IDD_JOINDIALOG                  9
-#define IDD_DIALOG2                     103
-#define IDD_CLIENTDIALOG                103
-#define IDD_DIALOG3                     105
-#define IDD_SELSEATDIALOG               107
-#define IDD_ORDERDIALOG                 109
-#define IDD_LOGINDIALOG                 110
-#define IDR_MENU1                       111
-#define IDC_IDEDIT                      1001
-#define IDC_IDLOGINEDIT                 1001
-#define IDC_NICKEDIT                    1002
-#define IDC_CHATCLIENTLIST              1003
-#define IDC_PASSEDIT                    1003
-#define IDC_PASSLOGINEDIT               1003
-#define IDC_JOINORLOGINBUTTON           1007
-#define IDC_REJOINBUTTON                1008
-#define IDC_REJOINORLOGINBUTTON         1008
-#define IDC_MSGCLIENTEDIT               1009
-#define IDC_SENDMSGCLIENTBUTTON         1010
-#define IDC_IPCLIENTEDIT                1017
-#define IDC_PORTCLIENTEDIT              1018
-#define IDC_ENTERCLIENTBUTTON           1019
-#define IDC_EDIT1                       1020
-#define IDC_SEATEDIT                    1020
-#define IDC_COUNTEDIT                   1020
-#define IDC_PCTIMEEDIT                  1020
-#define IDC_PNEDIT                      1020
-#define IDC_SEATBUTTON                  1021
-#define IDC_ADDREDIT                    1021
-#define IDC_ITEMLIST                    1022
-#define IDC_BIRTHEDIT                   1022
-#define IDC_JOINBUTTON                  1026
-#define IDC_LOGINBUTTON                 1027
-#define IDC_ORDERBUTTON                 1028
-#define IDC_GOORDERBUTTON               1028
-#define IDC_EXITBUTTON                  1029
-#define IDC_BUTTON1                     1030
-#define IDC_DELBUTTON                   1030
-#define IDC_UPDATEBUTTON                1030
-#define IDC_IDJOINEDIT                  1032
-#define IDC_PASSJOINEDIT                1033
-#define IDC_GOLOGINBUTTON               1034
-#define IDC_GOJOINBUTTON                1035
-#define ID_40001                        40001
-#define ID_40002                        40002
-#define ID_40003                        40003
-#define ID_40004                        40004
-#define ID_40005                        40005
-#define ID_40006                        40006
-#define ID_                             40007
-#define ID_M_JOIN                       40008
-#define ID_M_LOGIN                      40009
-#define ID_M_SELSEAT                    40010
-#define ID_M_ORDER                      40011
-#define ID_M_CONNECT                    40012
-#define ID_M_DISCONNECT                 40013
-#define ID_M_JOIN_OR_UPDATE             40014
+#define IDR_MENU1                       100
+#define ID_E_CHAT 101			//프로시저의 채팅 에디트 식별자
+#define ID_B_LOGOUT 102	//프로시저의 나가기 버튼 식별자
+#define ID_B_DELETE 103		//프로시저의 탈퇴 버튼 식별자
+#define ID_E_TIMER 104		//프로시저의 타이머 에디트 식별자
+#define ID_E_IP 105				//연결의 IP 에디트 식별자
+#define ID_E_PORT 106			//연결의 PORT 에디트 식별자
+#define ID_B_CONNECT 107	//연결의 연결 버튼 식별자
+#define ID_E_ID 108				//가입/수정의 아이디 에디트 식별자
+#define ID_E_PW 109			//가입/수정의 비번 에디트 식별자
+#define ID_E_NAME 110		//가입/수정의 이름 에디트 식별자
+#define ID_E_PN 111				//가입/수정의 전화번호 에디트 식별자
+#define ID_E_ADDR 112			//가입/수정의 주소 에디트 식별자
+#define ID_E_BIRTH 113		//가입/수정의 생일 에디트 식별자
+#define ID_B_JOIN 114			//가입/수정의 가입 버튼 식별자
+#define ID_B_UPDATE 115		//가입/수정의 수정 버튼 식별자
+#define ID_E_LOGIN_ID 116//로그인의 아이디 에디트 식별자
+#define ID_E_LOGIN_PW 117//로그인의 비번 에디트 식별자
+#define ID_B_LOGIN 118		//로그인의 로그인 버튼 식별자
+#define ID_E_SN 119				//자리의 자리번호 에디트 식별자
+#define ID_B_SELSEAT 120	//자리의 자리번호선택 버튼 식별자
+#define ID_L_ITEM 121			//주문의 아이템리스트 리스트박스 식별자
+#define ID_E_ORDERCOUNT 122	//주문의 아이템주문갯수 에디트 식별자
+#define ID_B_ORDER 123				//주문의 아이템 주문 버튼 식별자
+#define ID_S_IP 124						//연결의 IP 스태틱 식별자
+#define ID_S_PORT 125					//연결의 PORT 스태틱 식별자
+#define ID_S_ID 126						//가입/수정의 아이디 스태틱 식별자
+#define ID_S_PW 127					//가입/수정의 비번 스태틱 식별자
+#define ID_S_NAME 128				//가입/수정의 이름 스태틱 식별자
+#define ID_S_PN 129						//가입/수정의 전화번호 스태틱 식별자
+#define ID_S_ADDR 130				//가입/수정의 주소 스태틱 식별자
+#define ID_S_BIRTH 131				//가입/수정의 생일 스태틱 식별자
+#define ID_S_LOGIN_ID 132		//로그인의 아이디 스태틱 식별자
+#define ID_S_LOGIN_PW 133		//로그인의 비번 스태틱 식별자
+#define ID_S_SN 134					//자리의 자리번호 스태틱 식별자
+#define ID_S_ITEM 135				//주문의 아이템 스태틱 식별자
+#define ID_S_ORDERCOUNT 136	//주문의 아이템주문갯수 스태틱 식별자
+#define ID_S_CHAT 137				//프로시저의 채팅 스태틱 식별자
+#define ID_S_TIMER 138				//프로시저의 타이머 스태틱 식별자
+#define ID_M_LOGIN                      40009	//프로시저의 로그인 메뉴 식별자
+#define ID_M_SELSEAT                    40010	//프로시저의 자리선택 메뉴 식별자
+#define ID_M_ORDER                      40011		//프로시저의 주문 메뉴 식별자
+#define ID_M_CONNECT                    40012		//프로시저의 연결 메뉴 식별자
+#define ID_M_DISCONNECT                 40013	//프로시저의 해제 메뉴 식별자
+#define ID_M_JOIN_OR_UPDATE             40014		//프로시저의  가입/수정 메뉴 식별자
 
 // Next default values for new objects
 // 
