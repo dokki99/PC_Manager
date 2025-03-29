e99dd6316e5261d99b3474598aa9419c8be20ec4
JDH <eric5711@naver.com>
2025-03-29 17:36:07 +0900

    LoginPage 컨트롤 제작 및 위치 조정

--- Client_B/Client_B/Client_B/main.cpp
+++ Client_B/Client_B/Client_B/main.cpp
@@ -8,7 +8,7 @@ BOOL CALLBACK SeatDlgProc(HWND, UINT, WPARAM, LPARAM);
 DWORD WINAPI Recv_Thread(LPVOID);
 
 void Split_C_T(TCHAR*, TCHAR*);
-void Update_Seat(TCHAR *);
+void Update_Seat(TCHAR*);
 void Change_Screen(int);
 void Send_Text(const char*, const char*);
 
@@ -50,7 +50,7 @@ TCHAR buf[buflen];
 // 컨트롤 관련 변수
 
 HWND SEAT_BUTTON[MAX_SEAT];
-HWND JOIN_BTN, LOGIN_BTN, SELECTION_BTN;
+HWND JOIN_BTN, LOGIN_BTN, FINDID_BTN, FINDPW_BTN, SELECTION_BTN;
 HWND ID_EDIT, PW_EDIT;
 HWND STATIC_TEXT_ID, STATIC_TEXT_PW;
 
@@ -79,7 +79,6 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpszCmd
 	RegisterClass(&WndClass);
 
 	hWnd = CreateWindow(MainClass, MainClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
-
 	ShowWindow(hWnd, nCmdShow);
 
 	while (GetMessage(&Message, NULL, 0, 0)) {
@@ -95,16 +94,13 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpszCmd
  MainWndProc: 메인 윈도우 프로시저
 --------------------------------------------------------*/
 LRESULT CALLBACK MainWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
-	HDC hDC;
 	PAINTSTRUCT ps;
 	static HBRUSH hBrush = NULL;
-
 
 	switch (iMessage) {
 	case WM_CREATE:
 		hWndMain = hWnd;
 		hBrush = CreateSolidBrush(RGB(218, 220, 214));
-
 		//// 현재 접속 유저 아이디 초기화
 		//lstrcpy(hUser_ID, "");
 
@@ -134,19 +130,22 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPa
 
 		// ID / PW 입력창 만들기
 
-		STATIC_TEXT_ID = CreateWindow("static", "ID ", WS_CHILD | WS_VISIBLE | SS_CENTER , 200, 200, 40, 30, hWndMain, (HMENU)1, g_hInst, NULL);
-		STATIC_TEXT_PW = CreateWindow("static", "PW ", WS_CHILD | WS_VISIBLE | SS_CENTER, 200, 250, 40, 30, hWndMain, (HMENU)2, g_hInst, NULL);
+		STATIC_TEXT_ID = CreateWindow("static", "ID ", WS_CHILD | WS_VISIBLE | SS_CENTER, 15, 12, 40, 30, hWndMain, (HMENU)1, g_hInst, NULL);
+		STATIC_TEXT_PW = CreateWindow("static", "PW ", WS_CHILD | WS_VISIBLE | SS_CENTER, 15, 52, 40, 30, hWndMain, (HMENU)2, g_hInst, NULL);
 
-		ID_EDIT = CreateWindow("edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 240, 200, 200, 25, hWndMain, (HMENU)3, g_hInst, NULL);
-		PW_EDIT = CreateWindow("edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 240, 250, 200, 25, hWndMain, (HMENU)4, g_hInst, NULL);
+		ID_EDIT = CreateWindow("edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 55, 10, 200, 25, hWndMain, (HMENU)3, g_hInst, NULL);
+		PW_EDIT = CreateWindow("edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 55, 50, 200, 25, hWndMain, (HMENU)4, g_hInst, NULL);
 
 		// 회원가입/로그인/좌석선택 버튼 생성
 
-		JOIN_BTN = CreateWindow("button", "회원 가입", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 0, 100, 50, hWndMain, (HMENU)UTIL_BTN_ID, g_hInst, NULL);
-		LOGIN_BTN = CreateWindow("button", "로그인", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 0, 100, 50, hWndMain, (HMENU)(UTIL_BTN_ID + 1), g_hInst, NULL);
-		SELECTION_BTN = CreateWindow("button", "좌석 선택", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 0, 100, 50, hWndMain, (HMENU)(UTIL_BTN_ID + 2), g_hInst, NULL);
+		LOGIN_BTN = CreateWindow("button", "로그인", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 15, 80, 240, 30, hWndMain, (HMENU)UTIL_BTN_ID, g_hInst, NULL);
+		FINDID_BTN = CreateWindow("button", "ID찾기", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 15, 115, 80, 30, hWndMain, (HMENU)(UTIL_BTN_ID + 1), g_hInst, NULL);
+		FINDPW_BTN = CreateWindow("button", "PW찾기", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 95, 115, 80, 30, hWndMain, (HMENU)(UTIL_BTN_ID + 2), g_hInst, NULL);
+		JOIN_BTN = CreateWindow("button", "회원 가입", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 175, 115, 80, 30, hWndMain, (HMENU)(UTIL_BTN_ID + 3), g_hInst, NULL);
+		SELECTION_BTN = CreateWindow("button", "좌석 선택", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 0, 100, 50, hWndMain, (HMENU)(UTIL_BTN_ID + 4), g_hInst, NULL);
 
-		ShowWindow(SELECTION_BTN, SW_HIDE);
+		//초기화면인 LoginPage로 Screen 세팅
+		Change_Screen(0);
 		InvalidateRect(hWndMain, NULL, TRUE);
 
 		return 0;
@@ -155,19 +154,8 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPa
 
 		return (INT_PTR)hBrush;
 	case WM_COMMAND:
-		switch (LOWORD(wParam)){
+		switch (LOWORD(wParam)) {
 		case UTIL_BTN_ID:
-			// 회원 가입
-			GetWindowText(ID_EDIT, hUser_ID, 30);
-			GetWindowText(PW_EDIT, hUser_PW, 30);
-
-			if (lstrcmp(hUser_ID, "") != 0 && lstrcmp(hUser_PW,"") != 0) {
-				SetWindowText(ID_EDIT, "");
-				SetWindowText(PW_EDIT, "");
-				Change_Screen(1);
-			}
-			break;
-		case UTIL_BTN_ID + 1:
 			// 로그인
 			GetWindowText(ID_EDIT, hUser_ID, 30);
 			GetWindowText(PW_EDIT, hUser_PW, 30);
@@ -178,12 +166,31 @@ LRESULT CALLBACK MainWndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPa
 				Change_Screen(1);
 			}
 			break;
+		case UTIL_BTN_ID + 1:
+			//ID찾기
+
+			break;
 		case UTIL_BTN_ID + 2:
+			//PW찾기
+
+			break;
+		case UTIL_BTN_ID + 3:
+			// 회원 가입
+			GetWindowText(ID_EDIT, hUser_ID, 30);
+			GetWindowText(PW_EDIT, hUser_PW, 30);
+
+			if (lstrcmp(hUser_ID, "") != 0 && lstrcmp(hUser_PW, "") != 0) {
+				SetWindowText(ID_EDIT, "");
+				SetWindowText(PW_EDIT, "");
+				Change_Screen(1);
+			}
+			break;
+		case UTIL_BTN_ID + 4:
 			// 자리 선택
 			DialogBox(g_hInst, MAKEINTRESOURCE(IDD_DLG_SEAT), hWnd, (DLGPROC)SeatDlgProc);
 			break;
 		default:
-			
+
 			break;
 		}
 		return 0;
@@ -251,13 +258,13 @@ DWORD WINAPI Recv_Thread(LPVOID Param) {
 
 		nReturn = recv(*P, buf, 1024, 0);
 
-		if (nReturn != 0 && nReturn != SOCKET_ERROR) {			
+		if (nReturn != 0 && nReturn != SOCKET_ERROR) {
 			Split_C_T(CODE, TEXT);
 
 			if (lstrcmp(CODE, "S00") == 0) {
 				Update_Seat(TEXT);
 			}
-			else if(lstrcmp(CODE, "S01") == 0){
+			else if (lstrcmp(CODE, "S01") == 0) {
 				if (lstrcmp(TEXT, "SUCCESS") == 0) {
 					MessageBox(hWndMain, "success", "!", MB_OK);
 				}
@@ -306,7 +313,7 @@ void Update_Seat(TCHAR* TEXT) {
 	int i;
 	for (i = 0; i < MAX_SEAT + 1; i++) {
 		if (TEXT[i] == '0') {
-			if(!IsWindowEnabled(SEAT_BUTTON[i])) EnableWindow(SEAT_BUTTON[i], TRUE);
+			if (!IsWindowEnabled(SEAT_BUTTON[i])) EnableWindow(SEAT_BUTTON[i], TRUE);
 		}
 		else if (TEXT[i] == '1') {
 			if (IsWindowEnabled(SEAT_BUTTON[i])) EnableWindow(SEAT_BUTTON[i], FALSE);
@@ -316,12 +323,20 @@ void Update_Seat(TCHAR* TEXT) {
 }
 
 /*--------------------------------------------------------
- Change_Screen(int ) : 화면 변경
+ Change_Screen(int state) : 화면 변경
+ ------------(state)------------
+ 0 : LoginPage
+ 1 : PrincePage
 -------------------------------------------------------- */
 void Change_Screen(int state) {
+	RECT LoginPage{ 0,0,280,195 };
+	RECT PricePage{ 0,0,700,800 };
 	if (state == 0) {
-		ShowWindow(JOIN_BTN, SW_SHOW);
+		SetWindowPos(hWndMain, NULL, LoginPage.left, LoginPage.top, LoginPage.right, LoginPage.bottom, SWP_NOMOVE | SWP_NOZORDER);
 		ShowWindow(LOGIN_BTN, SW_SHOW);
+		ShowWindow(FINDID_BTN, SW_SHOW);
+		ShowWindow(FINDPW_BTN, SW_SHOW);
+		ShowWindow(JOIN_BTN, SW_SHOW);
 		ShowWindow(SELECTION_BTN, SW_HIDE);
 		ShowWindow(STATIC_TEXT_ID, SW_SHOW);
 		ShowWindow(STATIC_TEXT_PW, SW_SHOW);
@@ -329,8 +344,10 @@ void Change_Screen(int state) {
 		ShowWindow(PW_EDIT, SW_SHOW);
 	}
 	else if (state == 1) {
-		ShowWindow(JOIN_BTN, SW_HIDE);
 		ShowWindow(LOGIN_BTN, SW_HIDE);
+		ShowWindow(FINDID_BTN, SW_HIDE);
+		ShowWindow(FINDPW_BTN, SW_HIDE);
+		ShowWindow(JOIN_BTN, SW_HIDE);
 		ShowWindow(SELECTION_BTN, SW_SHOW);
 		ShowWindow(STATIC_TEXT_ID, SW_HIDE);
 		ShowWindow(STATIC_TEXT_PW, SW_HIDE);
